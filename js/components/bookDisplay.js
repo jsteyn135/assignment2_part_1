624f5b64b93f66e6f83b271d954a0298454a075d
jsteyn135 <jacques.zeus@gmail.com>
2021-03-17 04:16:18 -0600

    fixed the thumbnail undefined problem

--- js/components/bookDisplay.js
+++ js/components/bookDisplay.js
@@ -17,7 +17,7 @@ app.component('book-display', {
         <li class="list-group-item"> {{this.bookObj.volumeInfo.publisher}}, {{this.bookObj.volumeInfo.publishedDate}}</li>
         <li class="list-group-item" v-if="this.bookObj.volumeInfo.pageCount != undefined">page count: {{this.bookObj.volumeInfo.pageCount}}</li>
         <li class="list-group-item" v-if="this.bookObj.volumeInfo.printType != undefined">print Type: {{this.bookObj.volumeInfo.printType}}</li>
-        <li><img v-show="this.bookObj.volumeInfo.imageLinks.thumbnail != undefined" :src="this.bookObj.volumeInfo.imageLinks.thumbnail"></li>
+        <li><img  :src="this.thumbCheck" alt="woops no thumbnails!"></li>
         </ul>`,
 
     computed: {
@@ -28,14 +28,27 @@ app.component('book-display', {
                 return null;
         },
         authorCheck(){
+            
             var charactersArray = ["\[","\]","\"","\""];
             var theAuthors = JSON.stringify(this.bookObj.volumeInfo.authors);
+            if(this.bookObj.volumeInfo.authors == undefined){
+                return "NO AUTHORS FOUND!";
+
+            }
             for(let i=0;i<charactersArray.length;++i){
                 theAuthors = theAuthors.replace(charactersArray[i],"");
                 
             }//end of for
             return theAuthors;
         },//end of author check
+        thumbCheck(){
+            if(this.bookObj.volumeInfo.imageLinks == undefined){
+                return "images/noImage.png";
+            }
+            else{
+                return this.bookObj.volumeInfo.imageLinks.thumbnail;
+            }
+        }//end thumbcheck
         
 
     }
